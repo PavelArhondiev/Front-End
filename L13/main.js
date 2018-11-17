cd093e25cdec97b95c5c89d98488b7b4c9f0cb2a
PavelArhondiev <pavel.arhondiev@mail.bg>
2018-11-17 16:11:43 +0200

    ADD L14 homework

---
+++ L13/main.js
@@ -0,0 +1,11 @@
+var students = ["a", "b", "c", "d", "e"];
+console.log(students[1]);
+console.log(students[2]);
+console.log(students[3]);
+console.log(students[-4]);
+
+
+var a = students.splice(1,1);
+var b = students.splice(0,10);
+console.log(a);
+console.log(b);
