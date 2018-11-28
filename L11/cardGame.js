593b62f0ee9463d79cf388ffb4d8b6a6143aa637
PavelArhondiev <pavel.arhondiev@mail.bg>
2018-11-28 23:46:50 +0200

    scoring cards

--- L11/cardGame.js
+++ L11/cardGame.js
@@ -3,6 +3,12 @@ var firstCardClicked = '',
     firstCardID = '',
     secondCardID = '';
 
+var scoreCount = 0;
+var scoreCountDomNode = document.getElementById('scoreCount');
+var sessionMaxScore=localStorage.getItem('sessionMaxScore')
+var highScore=localStorage.getItem('highScoreKey');
+document.getElementById('highScoreCount').innerHTML=highScore;
+
 function cardFlipper(event, planetName) {
     showFace(event, planetName);
 
@@ -16,28 +22,36 @@ function cardFlipper(event, planetName) {
 
         if (firstCardClicked == secondCardClicked) {
             document.getElementById(firstCardID).classList.add('CardsHidden');
-            document.getElementById(firstCardID).classList.remove('Cards', 'faceDown');
             document.getElementById(secondCardID).classList.add('CardsHidden');
-            document.getElementById(secondCardID).classList.remove('Cards', 'faceDown');
             firstCardClicked = '',
                 secondCardClicked = '',
                 firstCardID = '',
                 secondCardID = '';
-        } else  {
+            scoreCount += 10;
+            localStorage.setItem('sessionMaxScore',scoreCount);
+            console.log(localStorage.sessionMaxScore)
+        } else {
             showBack();
             console.log(firstCardClicked);
             console.log(secondCardClicked);
-            
+
         }
     }
-
+    document.getElementById('scoreCount').innerHTML = scoreCount;
+    if (scoreCountDomNode.innerHTML > highScore) {
+        highScore = parseInt(scoreCount);
+        document.getElementById('highScoreCount').innerHTML = highScore;
+        localStorage.setItem('highScoreKey', highScore);
+    }
+    if(scoreCount>80){
+        alert('YOU WON');
+    }
 }
 function showFace(event, planetName) {
     urlString = "url(Images/" + planetName + ".png)";
     event.target.style.backgroundImage = urlString;
     event.target.classList.remove('faceDown');
 }
-
 function showBack() {
     document.getElementById(firstCardID).style.backgroundImage = "url(Images/CardBack.png";
     document.getElementById(firstCardID).classList.add('faceDown');
@@ -45,5 +59,12 @@ function showBack() {
         secondCardClicked = '',
         firstCardID = secondCardID,
         secondCardID = '';
-
 }
+document.querySelector('.reset').addEventListener('click', reloadGame);
+
+function reloadGame() {
+    location.reload();
+}
+
+setTimeout(function(){window.location.href='gameOver.html'},30000);
+
